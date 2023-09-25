cb793f4fa9dc67144f76b5300ab449505765807e
AdilAhmedShekhani <adilaziz2009@live.com>
2023-09-25 10:45:42 +0500

    error update

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res, next) => {
   try {
@@ -25,12 +26,15 @@ export const login = async (req, res, next) => {
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
-      user.password,
+      user.password
     );
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username!"));
 
-    res.status(200).json(user);
+      const token = jwt.sign({id:user._id, isAdmin:user.isAdmin}, "abcd")
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
