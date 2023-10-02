8cf822e639b492d7cb96bd0d770df311251d35c2
AdilAhmedShekhani <adilaziz2009@live.com>
2023-10-02 11:23:57 +0500

    User Updated

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,15 +1,5 @@
 import User from "../models/User.js";
 
-export const creatUser = async (req, res, next) => {
-  const newUser = new User(req.body);
-
-  try {
-    const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
-  } catch (err) {
-    next(err);
-  }
-};
 export const updateUser = async (req, res, next) => {
   try {
     const updateUser = await User.findByIdAndUpdate(
@@ -34,16 +24,16 @@ export const deleteUser = async (req, res, next) => {
 };
 export const getUser = async (req, res, next) => {
   try {
-    const User = await User.findById(req.params.id);
-    res.status(200).json(User);
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
 };
 export const getUsers = async (req, res, next) => {
   try {
-    const Users = await User.find();
-    res.status(200).json(Users);
+    const users = await User.find();
+    res.status(200).json(users);
   } catch (err) {
     next(err);
   }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,31 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../api/controllers/user.js";
+import { verifyToken, verifyUser } from "../api/utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+    res.send("hello user, you are logged in and you can delete your account")
+})
+
+//update
+router.put("/:id", updateUser);
+//delete
+router.delete("/:id", deleteUser);
+//get
+
+router.get("/:id", getUser);
+//get all
+
+router.get("/", getUsers);
 
 export default router;
